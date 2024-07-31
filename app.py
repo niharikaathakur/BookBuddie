7c7327472b8e023678b3118070ebf5434b8caf6f
Niharika Thakur <70130153+niharikaathakur@users.noreply.github.com>
2024-08-01 03:08:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 
-popular_df = pickle.load(open(r'C:\Users\Niharika Thakur\Desktop\Projects\book-recommender-system-master\book-recommender-system-master\popular.pkl','rb'))
+popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
 books = pickle.load(open('books.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
